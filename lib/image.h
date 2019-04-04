92c2fad311c24d4ecdc8e5a624e29c58609f874e
Namito Yokota <nyokota@crimson.ua.edu>
2019-04-04 10:54:44 -0500

    no duplicates

--- lib/image.h
+++ lib/image.h
@@ -5,31 +5,27 @@
 #include <stdlib.h>
 #include <string.h>
 
-typedef struct _pixel Pixel;
-typedef struct _imagePPM ImagePPM;
-typedef struct _imagePGM ImagePGM;
-
-struct _pixel {
+typedef struct _pixel {
   int red;
   int green;
   int blue;
-};
+} Pixel;
 
-struct _imagePPM {
+typedef struct _imagePPM {
 	char magic[3];  // magic identifier, "P3"
   int width;      // columns
   int height;     // rows
   int max_value;  // maximum intensity
   Pixel **pixels; // pixel data
-};
+} ImagePPM;
 
-struct _imagePGM {
+typedef struct _imagePGM {
 	char magic[3];  // magic identifier, "P2"
   int width;      // columns
   int height;     // rows
   int max_value;  // maximum grayscale intensity
   int **pixels;   // grayscale pixel data
-};
+} ImagePGM;
 
 ImagePPM *readPPM(char*);
 int writePGM(ImagePGM*, char*);
